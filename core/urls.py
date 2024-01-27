97a14c5752639550165ffce3f3f5cfac02faa329
omid <omedmm3@gmail.com>
2024-01-27 11:35:26 +0430

    added template and configured static and media

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
-
+from django.conf import settings
+from django.conf.urls.static import static
 from posts.views import index
 
 urlpatterns = [
@@ -8,3 +9,6 @@ urlpatterns = [
     path("blog/", include("posts.urls")),
     path("", index),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
