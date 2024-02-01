cccbf43598075c5ac8f115a47682ff3c94ca0def
omid <omedmm3@gmail.com>
2024-02-01 16:17:26 +0430

    added thumbnail to post model

--- posts/models.py
+++ posts/models.py
@@ -5,5 +5,6 @@ from django.db import models
 
 # Create your models here.
 class Post(models.Model):
+    thumbnail = models.ImageField(upload_to="thumbnails")
     title = models.CharField(max_length=200)
     content = models.TextField()
